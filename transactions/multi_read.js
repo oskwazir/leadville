867c6a94b6038a580953ee35a0a091ce4d7cc94b
Omer Wazir <omer.wazir@gmail.com>
2015-02-22 18:15:04 -0700

    Multiple commands

---
+++ transactions/multi_read.js
@@ -0,0 +1,13 @@
+const redis = require('../redis');
+
+redis.multi()
+.set('key 1','value 1')
+.get('key 1')
+.set('key 2','value 2')
+.get('key 2')
+    .exec(function(err,results){
+    if(err){throw err;}
+
+    console.log(`terminated. results: ${results}`);
+    redis.quit();
+});
