1cdaa26d3e4f0e3627b012db5c76d649490f2b3b
Omer Wazir <omer.wazir@gmail.com>
2015-03-01 23:05:38 -0700

    Simple incrementing

---
+++ integers/incr.js
@@ -0,0 +1,14 @@
+const redis = require('../redis');
+
+
+function done(err, result){
+    if(err){ throw err; }
+
+    console.log(`new value: ${result}`);
+    redis.quit();
+}
+
+for(var i =0; i < 10; i++){
+    redis.incr('incr.js key', done);
+}
+
